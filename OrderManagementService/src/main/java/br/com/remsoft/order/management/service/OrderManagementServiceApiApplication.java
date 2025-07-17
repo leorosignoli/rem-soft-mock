057236ff3c5320223450d4abc08dcd16a4bc2539
Leonardo Andrade <leonardo.andrade6@hotmail.com>
2025-07-17 17:00:57 -0300

    Base Project

---
+++ OrderManagementService/src/main/java/br/com/remsoft/order/management/service/OrderManagementServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.remsoft.order.management.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementServiceApiApplication.class, args);
+	}
+
+}
---
+++ OrderManagementService/src/test/java/br/com/remsoft/order/management/service/OrderManagementServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.remsoft.order.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
